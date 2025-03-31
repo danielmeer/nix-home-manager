d6ef09718fdf3638f429e94e941b97de0eafa051
Daniel Meer <daniel.meer@speedgoat.ch>
2025-03-31 12:38:35 +0200

    Add more packages and configurations

--- home.nix
+++ home.nix
@@ -18,18 +18,31 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # bmap-tools # Needs sudo
+    # dcfldd # Needs sudo
+    distrobox
+    duf
     fd
     fzf
     imagemagick
+    meld
+    patchelf
+    podman
     ripgrep
+    subversionClient
     tig
+    tio
+    tldr
     tree
+    ubootTools
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # nerdfonts
+    # (pkgs.nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
@@ -38,6 +51,8 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
+
+  # fonts.fontconfig.enable = true;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -71,15 +86,53 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
+    # EDITOR = "nvim";
+    # VISUAL = "nvim";
   };
+
+  # targets.genericLinux.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # programs.bash.enable = true;
+  # xdg.mime.enable = true;
+  # xdg.systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share/applications" ];
+
+  # programs.bash = {
+  #   enable = true;
+  #   profileExtra = ''
+  #     export XDG_DATA_DIRS=$HOME/.home-manager-share:$XDG_DATA_DIRS
+  #   '';
+  # };
+
+  # home.activation = {
+  #   linkDesktopApplications = {
+  #     after = [ "writeBoundary" "createXdgUserDirectories" ];
+  #     before = [ ];
+  #     data = ''
+  #       rm -rf $HOME/.home-manager-share
+  #       mkdir -p $HOME/.home-manager-share
+  #       cp -Lr --no-preserve=mode,ownership ${config.home.homeDirectory}/.nix-profile/share/* $HOME/.home-manager-share
+  #     '';
+  #   };
+  # };
+
+  programs.bat.enable = true;
+
+  programs.eza = {
+    enable = true;
+    extraOptions = [
+      "--group"
+      "--group-directories-first"
+    ];
+  };
+
   programs.fish = {
     enable = true;
+    functions = {
+      netio = "curl \"http://DaMe-Netio/netio.cgi?pass=netio&output{$argv}=4\"";
+    };
     shellAliases = {
       ls = "ls --color=auto --group-directories-first";
       t = "tig --all";
@@ -88,6 +141,9 @@
       gdbc = "git difftool --tool=bc";
       gitmerge = "git mergetool --tool=meld";
     };
+    plugins = [
+      { name = "bass"; src = pkgs.fishPlugins.bass.src; }
+    ];
   };
 
   programs.git = {
@@ -95,7 +151,10 @@
     package = pkgs.gitFull;
     userName = "Daniel Meer";
     userEmail = "daniel.meer@speedgoat.ch";
+    lfs.enable = true;
     extraConfig = {
+      # Hide warning in git-svn when 'git dcommit' first pulls new updates
+      advice.skippedCherryPicks = "false";
       # core.editor = "code --wait";
       credential.helper = "libsecret";
       diff = {
@@ -108,5 +167,34 @@
       mergetool.vscode.cmd = "code --wait --merge $REMOTE $LOCAL $BASE $MERGED";
       pull.rebase = "true";
     };
+    # delta = {
+    #   enable = true;
+    #   options = {
+    #     decorations = {
+    #       commit-decoration-style = "bold yellow box ul";
+    #       file-decoration-style = "none";
+    #       file-style = "bold yellow ul";
+    #     };
+    #     features = "decorations";
+    #   };
+    # };
   };
+
+  programs.lazygit.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraConfig = ''
+      autocmd FileType gitcommit set textwidth=80
+    '';
+    vimAlias = true;
+  };
+
+  programs.nnn = {
+    enable = true;
+    package = pkgs.nnn.override ({ withNerdIcons = true; });
+  };
+
+  programs.password-store.enable = true;
 }
